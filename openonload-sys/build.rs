dbd01cd9cbc8d51681956613fcde12977782fefa
Jim McGrath <jimmc2@gmail.com>
2017-08-31 21:47:32 -0500

    remove some commented out code

--- openonload-sys/build.rs
+++ openonload-sys/build.rs
@@ -5,18 +5,10 @@ use std::path::PathBuf;
 
 fn main() {
     let bindings = bindgen::Builder::default()
-//        .no_unstable_rust()
         .header("wrapper.h")
-//        .clang_arg("-I../../libs/toyframe")
-//        .clang_arg("-I../../libs/sutl")
-//        .clang_arg("-I../../services/pfrechan/include")
-//        .clang_arg("--std=c++11")
-//        .whitelisted_type("rtpx::.*")
-//        .whitelisted_type("pfrechanmsgs::.*")
-//        .enable_cxx_namespaces()
         // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
+        // Unwrap the Result or panic on failure.
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
